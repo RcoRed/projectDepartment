3235fab022a112c2fe2db6bb673aa15c0b8b7e72
RcoRed <notpurple.red@gmail.com>
2023-03-11 22:51:13 +0100

    rifattorizza il codice parte 2

--- src/main/java/org/generation/italy/projectDepartment/model/data/abstractions/lambdaRawMapperInterface.java
+++
@@ -1,9 +0,0 @@
-package org.generation.italy.projectDepartment.model.data.abstractions;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-@FunctionalInterface
-public interface lambdaRawMapperInterface {
-    Object lambdaRawMapper(ResultSet rs) throws SQLException;
-}
